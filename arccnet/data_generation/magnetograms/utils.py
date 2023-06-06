2d090fc446dea5ce97cd712688ff5ae8077e749d
Paul Wright <paul@pauljwright.co.uk>
2023-06-06 14:56:37 +0100

    magnetogram utils + tests

---
+++ arccnet/data_generation/magnetograms/utils.py
@@ -0,0 +1,41 @@
+import datetime
+
+import arccnet.data_generation.utils.default_variables as dv
+
+__all__ = ["datetime_to_jsoc", "jsoc_to_datetime"]
+
+
+def datetime_to_jsoc(datetime_obj: datetime.datetime):
+    """
+    jsoc string from datetime object
+
+    Parameters
+    ----------
+    datetime_obj : datetime.datetime
+        a datetime object to convert to JSOC format
+
+    Returns
+    -------
+    string
+        string of format `dv.JSOC_DATE_FORMAT`
+
+    """
+    return datetime_obj.strftime(dv.JSOC_DATE_FORMAT)
+
+
+def jsoc_to_datetime(date_string: str):
+    """
+    datetime object from jsoc string
+
+    Parameters
+    ----------
+    datetime_obj : datetime.datetime
+        a JSOC format string to convert to datetime object
+
+    Returns
+    -------
+    datetime.datetime
+        ...
+
+    """
+    return datetime.datetime.strptime(date_string, dv.JSOC_DATE_FORMAT)
