eed1224ead9c08a5d84245cfbd26d075b61d9324
Paul Wright <paul@pauljwright.co.uk>
2023-07-20 12:45:33 +0100

    testing cropped maps

--- arccnet/data_generation/mag_processing.py
+++ arccnet/data_generation/mag_processing.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
+
+import astropy.units as u
+from astropy.coordinates import SkyCoord
 
 import arccnet.data_generation.utils.default_variables as dv
 import sunpy.map
@@ -53,7 +57,7 @@ class MagnetogramProcessor:
         # 2. set data off-disk to 0 (np.nan would be ideal, but deep learning)
         r_map.data[~sunpy.map.coordinate_is_on_solar_disk(sunpy.map.all_coordinates_from_map(r_map))] = 0
 
-        # !TODO ...
+        # !TODO
         # 3. normalise radius to fixed value
         # 4. ...
 
@@ -74,6 +78,62 @@ class MagnetogramProcessor:
         return rmap
 
 
+class ARExtractor:
+    def __init__(self) -> None:
+        filename = Path(dv.MAG_INTERMEDIATE_DATA_CSV)
+
+        if filename.exists():
+            self.loaded_data = pd.read_csv(filename)
+
+        columns_to_update = ["url_hmi", "url_mdi"]
+        new_columns = ["processed_hmi", "processed_mdi"]
+
+        dv_base_path = Path("/Users/pjwright/Documents/work/ARCCnet/data/02_intermediate/mag/fits/")
+        dv_process_path = Path("/Users/pjwright/Documents/work/ARCCnet/data/03_processed/mag/fits/")
+        # Iterate through the columns and update the paths
+        for old_column, new_column in zip(columns_to_update, new_columns):
+            self.loaded_data[new_column] = self.loaded_data[old_column].map(
+                lambda x: dv_base_path / Path(x).name if pd.notna(x) else x
+            )
+
+        for index, row in self.loaded_data.iterrows():
+            if self.loaded_data.iloc[index]["processed_hmi"] is np.nan:
+                pass
+            else:
+                lat = self.loaded_data.iloc[index]["Latitude"]
+                lng = self.loaded_data.iloc[index]["Longitude"]
+                hmi = self.loaded_data.iloc[index]["processed_hmi"]
+                time = self.loaded_data.iloc[index]["datetime_hmi"]
+
+                dt = self.loaded_data.iloc[index]["datetime_srs"]
+                numbr = self.loaded_data.iloc[index]["Number"]
+
+                my_hmi_map = sunpy.map.Map(hmi)
+
+                _cd = SkyCoord(
+                    lng * u.deg,
+                    lat * u.deg,
+                    obstime=time,
+                    frame=sunpy.coordinates.frames.HeliographicStonyhurst,
+                )
+
+                transformed = _cd.transform_to(my_hmi_map.coordinate_frame)
+
+                tr_x = transformed.helioprojective.Tx + 200 * u.arcsec
+                tr_y = transformed.helioprojective.Ty + 100 * u.arcsec
+
+                bl_x = transformed.helioprojective.Tx - 200 * u.arcsec
+                bl_y = transformed.helioprojective.Ty - 100 * u.arcsec
+
+                top_right = SkyCoord(tr_x, tr_y, frame=my_hmi_map.coordinate_frame)
+                bottom_left = SkyCoord(bl_x, bl_y, frame=my_hmi_map.coordinate_frame)
+                my_hmi_submap = my_hmi_map.submap(bottom_left, top_right=top_right)
+
+                print(dv_process_path / f"{dt}_{numbr}.fits")
+                my_hmi_submap.save(dv_process_path / f"{dt}_{numbr}.fits")
+
+
 if __name__ == "__main__":
     logger.info(f"Executing {__file__} as main program")
     _ = MagnetogramProcessor()
+    _ = ARExtractor()
