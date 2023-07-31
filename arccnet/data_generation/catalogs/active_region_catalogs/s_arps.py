136dc58ad86f3b2e9742dd4124a4cb406b5456b2
Paul Wright <paul@pauljwright.co.uk>
2023-07-31 22:46:34 +0100

    placeholder s-arps catalog

---
+++ arccnet/data_generation/catalogs/active_region_catalogs/s_arps.py
@@ -0,0 +1,26 @@
+from arccnet.data_generation.catalogs.base_catalog import BaseCatalog
+
+__all__ = ["SARPSCatalog"]
+
+
+class SARPSCatalog(BaseCatalog):
+    """
+    SARPSCatalog is a class for fetching and processing SHARPs/SMARPs active region
+    classification data.
+
+    It provides methods to fetch data from JSOC, create a catalog from the
+    fetched data, clean and validate the catalog, and perform other related
+    operations.
+    """
+
+    def __init__(self):
+        pass
+
+    def create_catalog(self):
+        pass
+
+    def clean_catalog(self):
+        pass
+
+    def fetch_data(self):
+        pass
